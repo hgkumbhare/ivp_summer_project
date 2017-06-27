ee2503844b0d18289ddca3746b9e3732ebdccbcd
hgkumbhare <hgkumbhare@gmail.com>
2017-06-27 22:53:40 +0530

    commented the code

--- Hadoop Image Processing Interface/src/org/hipi/examples/HelloWorld.java
+++ Hadoop Image Processing Interface/src/org/hipi/examples/HelloWorld.java
@@ -1,162 +1,131 @@
 package org.hipi.examples;
 
-import org.hipi.image.FloatImage;
-import org.hipi.image.HipiImageHeader;
-import org.hipi.imagebundle.mapreduce.HibInputFormat;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import java.util.concurrent.TimeUnit;
+import javax.imageio.ImageIO;
 
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-import java.io.IOException;
-
-import java.awt.*;  
-import javax.swing.JFrame;  
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.hipi.image.FloatImage;
+import org.hipi.image.HipiImageHeader;
+import org.hipi.imagebundle.mapreduce.HibInputFormat;
 
 public class HelloWorld extends Configured implements Tool {
-  
-public static int mykeyvalue=0;
 
-public static class HelloWorldMapper extends Mapper<HipiImageHeader, FloatImage, IntWritable, FloatImage> {
-    
-    public void map(HipiImageHeader key, FloatImage value, Context context) 
-        throws IOException, InterruptedException {
+    public static int index = 1;
 
-	System.out.println("INSIDE MAPPER");
-      // Verify that image was properly decoded, is of sufficient size, and has three color channels (RGB)
-      if (value != null && value.getWidth() > 1 && value.getHeight() > 1 && value.getNumBands() == 3) {
+    public static class HelloWorldMapper extends
+	    Mapper<HipiImageHeader, FloatImage, IntWritable, FloatImage> {
+	public void map(HipiImageHeader key, FloatImage value, Context context)
+		throws IOException, InterruptedException {
 
-        // Emit record to reducer
-	mykeyvalue+=1;
-        context.write(new IntWritable(mykeyvalue), value);
-
-      } // If (value != null...
-      
-    } // map()
-
-  } // HelloWorldMapper
-
-
-public static class HelloWorldReducer extends Reducer<IntWritable, FloatImage, IntWritable, FloatImage> {
-
-    public void reduce(IntWritable key, Iterable<FloatImage> values, Context context)
-        throws IOException, InterruptedException {
-
-	System.out.println("INSIDE REDUCER");
-
-      for (FloatImage value : values) {
-	 // Get dimensions of image
-        int w = value.getWidth();
-        int h = value.getHeight();
-
-        // Get pointer to image data
-        float[] valData = value.getData();
-
-        // Initialize w*h element float array to hold gray pixel value
-	float[] grayData = new float[w*h];
-
-        // Traverse image pixel data in raster-scan order and update
-        for (int j = 0; j < h; j++) {
-          for (int i = 0; i < w; i++) {
-            grayData[j*w+i] = (float)0.299*valData[(j*w+i)*3+0]+(float)0.587*valData[(j*w+i)*3+1]+(float)0.114*valData[(j*w+i)*3+2]; // RGB
-          }
-        }
-	
-	FloatImage finalData = new FloatImage(h, w, 1,grayData);
-
-	System.out.println("HELLO wt " + finalData.getWidth() + " height "+finalData.getHeight());
-
-
-        // Emit output of job which will be written to HDFS
-        context.write(key, finalData);
-      }
-
-    } // reduce()
-
-  } // HelloWorldReducer
-
-  
-  public int run(String[] args) throws Exception {
-    // Check input arguments
-    if (args.length != 2) {
-      System.out.println("Usage: helloWorld <input HIB> <output directory>");
-      System.exit(0);
+	    context.write(new IntWritable(1), value);
+	    index++;
+	}
     }
-    
-    // Initialize and configure MapReduce job
-    Job job = Job.getInstance();
-    // Set input format class which parses the input HIB and spawns map tasks
-    job.setInputFormatClass(HibInputFormat.class);
-    // Set the driver, mapper, and reducer classes which express the computation
-    job.setJarByClass(HelloWorld.class);
-    job.setMapperClass(HelloWorldMapper.class);
-    job.setReducerClass(HelloWorldReducer.class);
-    // Set the types for the key/value pairs passed to/from map and reduce layers
-    job.setMapOutputKeyClass(IntWritable.class);
-    job.setMapOutputValueClass(FloatImage.class);
-    job.setOutputKeyClass(IntWritable.class);
-    job.setOutputValueClass(FloatImage.class);
-    
-    // Set the input and output paths on the HDFS
-    FileInputFormat.setInputPaths(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-    // Execute the MapReduce job and block until it complets
-    boolean success = job.waitForCompletion(true);
-    
-    // Return success or failure
-    return success ? 0 : 1;
-  }
+    public static class HelloWorldReducer extends
+	    Reducer<IntWritable, FloatImage, IntWritable, IntWritable> {
+	public void reduce(IntWritable key, Iterable<FloatImage> values,
+		Context context) throws IOException, InterruptedException {
+	    int index = 1;
+	    for (FloatImage value : values) {
+		float[] dataBuff = value.getData();
 
+		BufferedImage bufferedImage = new BufferedImage(
+			value.getWidth(), value.getHeight(),
+			BufferedImage.TYPE_INT_RGB);
 
+		// Hipi gives input images in FloatImage format.
+		// Before processing these images we need to convert them into a
+		// format that allows us to perform basic image procesing.
+		// Convert FloatImage to BufferedImage.
+		for (int i = 0; i < value.getHeight(); i++) {
+		    for (int j = 0; j < value.getWidth(); j++) {
+			// red component of pixel
+			int r = Math
+				.round(dataBuff[3 * (i * value.getWidth() + j)]
+					* (float) 255.0);
+			// green component of pixel
+			int g = Math
+				.round(dataBuff[3 * (i * value.getWidth() + j) + 1]
+					* (float) 255.0);
+			// blue component of pixel
+			int b = Math
+				.round(dataBuff[3 * (i * value.getWidth() + j) + 2]
+					* (float) 255.0);
 
-public static class MyCanvas extends Canvas{  
-      
-    public void paint(Graphics g) {  
-  
-        Toolkit t=Toolkit.getDefaultToolkit();  
-        Image i=t.getImage("p3.gif");  
-        g.drawImage(i, 120,100,this);  
-          
-    }  
-  
-}  
+			// value of the Buffered Image pixel calculated from the
+			// rgb values of the FloatImage
+			int val = (r << 16) | (g << 8) | b;
+			bufferedImage.setRGB(j, i, val);
+		    }
+		}
+		// writing image in output directory given in jpeg format
 
+		// output directory needs to be mentioned.
+		ImageIO.write(bufferedImage, "jpg", new File(
+			"/home/hduser/outputdata50" + Integer.toString(index)
+				+ ".jpg"));
+		context.write(new IntWritable(index), new IntWritable(index));
+		index++;
+	    }
+	}
+    }
 
+    public int run(String[] args) throws Exception {
+	// Check input arguments
+	if (args.length != 2) {
+	    System.out
+		    .println("Usage: helloWorld <input HIB> <output directory>");
+	    System.exit(0);
+	}
 
+	// Initialize and configure MapReduce job
+	Job job = Job.getInstance();
+	// Set input format class which parses the input HIB and spawns map
+	// tasks
+	job.setInputFormatClass(HibInputFormat.class);
+	// Set the driver, mapper, and reducer classes which express the
+	// computation
+	job.setJarByClass(HelloWorld.class);
+	job.setMapperClass(HelloWorldMapper.class);
+	job.setReducerClass(HelloWorldReducer.class);
+	// Set the types for the key/value pairs passed to/from map and reduce
+	// layers
+	job.setMapOutputKeyClass(IntWritable.class);
+	job.setMapOutputValueClass(FloatImage.class);
+	job.setOutputKeyClass(IntWritable.class);
+	job.setOutputValueClass(IntWritable.class);
 
+	// Set the input and output paths on the HDFS
+	FileInputFormat.setInputPaths(job, new Path(args[0]));
+	FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+	// Execute the MapReduce job and block until it complets
+	boolean success = job.waitForCompletion(true);
 
+	// Return success or failure
+	return success ? 0 : 1;
+    }
 
-  public static void main(String[] args) throws Exception {
-	System.out.println("JUST INSIDE START");
-	long startTime = System.nanoTime();
-    ToolRunner.run(new HelloWorld(), args);
-	long difference = System.nanoTime() - startTime;
-	System.out.println("Total execution time: " + String.format("%d min, %d sec", TimeUnit.NANOSECONDS.toHours(difference), TimeUnit.NANOSECONDS.toSeconds(difference) - TimeUnit.MINUTES.toSeconds(TimeUnit.NANOSECONDS.toMinutes(difference))));
+    public static void main(String[] args) throws Exception {
+	long startTime = System.currentTimeMillis();
+	ToolRunner.run(new HelloWorld(), args);
+	long endTime = System.currentTimeMillis();
+	long totalTime = endTime - startTime;
+	System.out.println("SEE TIME HERE " + totalTime);
+	System.exit(0);
+    }
 
-
-	 MyCanvas m=new MyCanvas();  
-        JFrame f=new JFrame();  
-        f.add(m);  
-        f.setSize(400,400);  
-        f.setVisible(true);  
-
-
-
-    System.exit(0);
-  }
-  
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ivp_summer_project
 summer project
+
+To make folder to git repo
+https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+
